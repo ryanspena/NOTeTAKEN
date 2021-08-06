8c4f89ee6388d148169ff6a744bf17a3db65dd4c
Ryan Pena <ryanspena@gmail.com>
2021-08-06 10:36:55 -0500

    server edit

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+const PORT = process.env.PORT || 3002;
+
 const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const notes = require("./data/db.json")
 const htmlRoutes = require('./routes/htmlRoutes')
 const apiRoutes = require('./routes/apiRoutes')
-const app = express();
+
 
 app.get('./public/assets/js/index.js', (req, res) => {
     res.json(db.json);
 });
-app.use(express.json());
-
 app.use(express.static('public'));
-
 app.use('/routes/apiRoutes.js')
 app.use('/routes/htmlRoutes.js')
 app.listen(3002, () => {
