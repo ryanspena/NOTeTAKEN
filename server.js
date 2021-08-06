c9a70da5a25c1ffc3610c81a17a7805331512a5c
Ryan Pena <ryanspena@gmail.com>
2021-08-06 07:46:11 -0500

    routes edited

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
 const app = express();
 
 app.get('./public/assets/js/index.js', (req, res) => {
     res.json(db.json);
 });
+app.use(express.json());
 
+app.use(express.static('public'));
+
+app.use('/routes/apiRoutes.js')
+app.use('/routes/htmlRoutes.js')
 app.listen(3002, () => {
     console.log('lets see what happens?')
     return;
